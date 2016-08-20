f371219c7de2c36a878ca768599b4eabf733348e
robinroyer <robin.royer974@gmail.com>
2016-08-20 22:09:14 +0200

    FIX: we can now retrive a file from datomic

--- datomic-pro-0.9.5385/POC/script-poc.groovy
+++ datomic-pro-0.9.5385/POC/script-poc.groovy
@@ -1,27 +1,35 @@
 #!/usr/local/bin/groovy
-println "===================================== importing libs";
+println "===================================== Importing libs";
 
 import datomic.Peer
 import datomic.Connection
 import datomic.Util
 import org.apache.commons.io.IOUtils
 
+// CONSTANTE DECLARATIONS
+INPUT_DIRECTORY = "./POC/datomic_input/";
+OUTPUT_DIRECTORY = "./POC/datomic_output/";
+SCHEMA_PATH = "./POC/test-schema.edn";
 
-println "===================================== create and connect to the DB";
+println "===================================== Creating DB and Connecting to datomic";
 
 uri = "datomic:mem://coucou";
 Peer.createDatabase(uri);
 conn = Peer.connect(uri);
 
-println "===================================== load a db schema from file";
+
+
+println "===================================== Loading a db schema from file";
 
 reader = new FileReader("./POC/test-schema.edn");
 List tx = Util.readAll(reader).get(0);
 txResult = conn.transact(tx).get();
-//println txResult
 
 
-File file = new File("./POC/file_to_save.txt");
+
+println "===================================== Storing files to the datomic";
+
+File file = new File( INPUT_DIRECTORY + "file_to_save.txt");
 BufferedReader reader = new BufferedReader(new FileReader (file));
   String         line = null;
   StringBuilder  stringBuilder = new StringBuilder();
@@ -39,30 +47,31 @@ BufferedReader reader = new BufferedReader(new FileReader (file));
   }
 
 
-println data
 List tx2 = Util.list (Util.map (":db/id", 1, ":files/data", data));
-
-
 tx_Result = conn.transact(tx2).get();
-db = conn.db();
 
+
+
+println "===================================== Retrieving files from the datomic";
+
+db = conn.db();
 query = "[:find ?d :where [ _ :files/data ?d]]";
 results = Peer.q(query, db);
-println " =========> there is " + results.size() + " results: "
+println "    ===> there is " + results.size() + " results: "
+
 for (result in results) {
-  println result
+  string_data = result[0]
+  try{
+    PrintWriter out = new PrintWriter( OUTPUT_DIRECTORY + "output.txt" );
+    out.write( string_data );
+    out.close();
+  }
+  catch(e){
+    println "  /!\\ /!\\ /!\\ ERROR ON FILE WRITING /!\\ /!\\ /!\\ "
+  }
 }
 
 
 
-//tab = result.toArray();
-//db.entity.get(":files/data")
-//result.iterator().next().get(0);
-//byte[]cc = (byte[])data;
-//FileOutputStream fos = new FileOutputStream("cc");
-
-//fos.write(cc);
-//fos.close();
-
-println "===================================== end of the POC the file retrieved is xx.txt";
+println "===================================== End of the POC the file retrieved is xx.txt";
 System.exit(0)
