fee4c0c3dff83bd1979405fea2cd1fc8e8fc8a0f
robinroyer <robin.royer974@gmail.com>
2016-08-21 13:46:09 +0200

    script for init db in mem

---
+++ datomic-pro-0.9.5385/POC/init_my_db.groovy
@@ -0,0 +1,24 @@
+#!/usr/local/bin/groovy
+
+
+import datomic.Peer
+import datomic.Connection
+import datomic.Util
+
+SCHEMA_PATH = "./POC/test-schema.edn";
+uri = "datomic:mem://coucou";
+
+Peer.createDatabase(uri);
+conn = Peer.connect(uri);
+
+reader = new FileReader("./POC/test-schema.edn");
+List tx = Util.readAll(reader).get(0);
+txResult = conn.transact(tx).get();
+
+println "===================================== Making a partition";
+
+partition_tx = [["db/id": Peer.tempid(":db.part/db"),
+                 "db/ident": ":files",
+                 "db.install/_partition": "db.part/db"]];
+txResult = conn.transact(partition_tx).get();
+System.exit(0)
